9081987e0f546d5aaa43049f9095c151a967946b
arpitsingh21 <arpit006singhas@gmail.com>
2017-11-18 22:37:04 +0530

    first commit

---
+++ app/src/main/java/iband/medikoe/model/adapter/StarwarAdapter.java
@@ -0,0 +1,8 @@
+package iband.medikoe.model.adapter;
+
+/**
+ * Created by dell on 18-11-2017.
+ */
+
+public class StarwarAdapter {
+}
---
+++ app/src/main/java/iband/medikoe/presenter/StarwarController.java
@@ -0,0 +1,8 @@
+package iband.medikoe.presenter;
+
+/**
+ * Created by dell on 18-11-2017.
+ */
+
+public class StarwarController {
+}
